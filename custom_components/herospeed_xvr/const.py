97c274a25cb4170f065d17703a8603c0d47f3b95
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-10-29 20:10:10 +0100

    Create const.py

---
+++ custom_components/herospeed_xvr/const.py
@@ -0,0 +1,14 @@
+"""Constants for the Herospeed XVR integration."""
+DOMAIN = "herospeed_xvr"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_NUM_SENSORS = "num_sensors"
+CONF_MOTION_RESET_DELAY = "motion_reset_delay"
+CONF_SMTP_USERNAME = "smtp_username"
+CONF_SMTP_PASSWORD = "smtp_password"
+
+DEFAULT_PORT = 25
+DEFAULT_MOTION_RESET_DELAY = 20
+DEFAULT_SMTP_USERNAME = "test@test.com"
+DEFAULT_SMTP_PASSWORD = "testtest"
